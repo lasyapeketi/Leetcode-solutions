5ae0fb97d5764e1c9344818503feb31a13c67bc1
lasyapeketi <peketilasya@gmail.com>
2026-09-22 14:59:34 +0530

    Time: 0 ms (100%), Space: 11.6 MB (31.42%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        int n=senate.size();
+        queue<int> r,d;
+        for(int i=0;i<n;i++){
+            if(senate[i]=='R'){
+                r.push(i);
+            }
+            else
+                d.push(i);
+        }
+    
+        while(!r.empty() && !d.empty()){
+            int ri=r.front();
+            int di=d.front();
+
+            r.pop();
+            d.pop();
+
+            if(ri<di){
+                r.push(ri+n);
+            }
+            else{
+                d.push(di+n);
+            }
+
+        }
+        
+            if (r.empty())
+                return "Dire";
+
+
+            return "Radiant";
+    }
+};
